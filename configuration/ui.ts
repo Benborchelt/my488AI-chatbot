98a2eab65cf5e4f0254677f3f7cc0857af04e599
Benjamin Borchelt <123499413+Benborchelt@users.noreply.github.com>
2025-03-05 20:56:23 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `BracketBusterAI`;
+export const CHAT_HEADER: string = `BracketBoosterAI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Data Last Updated Wednesday March 5th, 2025 at 8:37Pm`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `BracketBoosterAI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
